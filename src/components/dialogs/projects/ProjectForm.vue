0b51d78fe2b91b54ceacc27b48a43c7ecc69aad0
Giorgos Papapanos <g.papapanos1996@gmail.com>
2025-05-24 19:39:46 +0300

    feat: projects crude

---
+++ src/components/dialogs/projects/ProjectForm.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-card class="flex-grow-1 d-flex flex-column">
+    <v-card-title class="d-flex align-center bg-primary-darken-1">
+      <div class="title">{{ project ? 'Edit Project' : 'Create Project' }}</div>
+      <v-spacer></v-spacer>
+      <v-btn icon="mdi-close" variant="text" @click="$emit('close')"></v-btn>
+    </v-card-title>
+    <div class="pa-8 scrollable flex-grow-1">
+      <v-form>
+        <v-text-field
+          v-model="form.name"
+          density="comfortable"
+          variant="outlined"
+          label="Name"
+        ></v-text-field>
+
+        <v-textarea
+          v-model="form.description"
+          density="comfortable"
+          variant="outlined"
+          label="Description"
+          rows="3"
+          auto-grow
+          class="mt-4"
+        ></v-textarea>
+
+        <v-switch
+          v-model="form.status"
+          color="primary"
+          label="Active"
+          class="mt-4"
+          hide-details
+        ></v-switch>
+      </v-form>
+    </div>
+
+    <v-card-actions class="my-4 mr-2">
+      <v-spacer></v-spacer>
+      <v-btn
+        color="primary"
+        variant="outlined"
+        text="Close"
+        @click="$emit('close')"
+        class="mr-2"
+      ></v-btn>
+      <v-btn color="primary" text="Save" variant="flat" @click="onSubmitProject"></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import { useBaseStore } from '@/stores/base'
+import { storeToRefs } from 'pinia'
+
+const { project } = defineProps({ project: Object })
+const emits = defineEmits(['close', 'reset'])
+
+const baseStore = useBaseStore()
+const { snackbar } = storeToRefs(baseStore)
+
+const form = ref({
+  name: null,
+  description: null,
+  status: true,
+})
+
+onMounted(() => {
+  if (project) {
+    form.value = { ...project }
+  }
+})
+
+const onSubmitProject = async () => {
+  try {
+    if (project) {
+      await axios.put(`/projects/${project.id}`, {
+        name: form.value.name,
+        description: form.value.description,
+        status: form.value.status,
+        locale: form.value.locale,
+      })
+    } else {
+      await axios.post('/projects', {
+        name: form.value.name,
+        description: form.value.description,
+        status: form.value.status,
+        locale: form.value.locale,
+      })
+    }
+
+    emits('reset')
+  } catch (error) {
+    snackbar.value = {
+      show: true,
+      text: `${error?.response?.data?.error?.issues?.[0]?.message || 'An error occurred'}`,
+      color: 'error',
+      icon: 'mdi-alert-circle-outline',
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/admin/projects/ProjectsList.vue
+++ src/views/admin/projects/ProjectsList.vue
@@ -1,9 +1,217 @@
 <template>
   <div class="component-wrapper d-flex flex-column">
-    <page-title title="Projects"></page-title>
+    <page-title title="Projects">
+      <v-btn-group variant="outlined" class="mr-3" density="comfortable">
+        <v-btn
+          @click="onOpenProjectFormDialog(null)"
+          color="primary"
+          icon="mdi-plus"
+          class="mr-2"
+          v-tooltip="'Create Project'"
+        ></v-btn>
+      </v-btn-group>
+
+      <v-text-field
+        color="primary"
+        append-inner-icon="mdi-magnify"
+        maxWidth="300px"
+        variant="outlined"
+        label="Search Projects"
+        clearable
+        hide-details
+        class="ml-auto"
+        density="compact"
+        @click:clear="
+          () =>
+            (filters = {
+              ...filters,
+              title: null,
+              page: 1,
+            })
+        "
+        @input="(e) => updateFilters(e.target.value)"
+      >
+      </v-text-field>
+    </page-title>
+
+    <v-data-table-server
+      :headers="headers"
+      :items="data?.projects"
+      :items-length="data?.pagination?.totalItems || 0"
+      item-value="id"
+      :loading="isLoading"
+      class="mx-auto mt-4"
+      style="max-width: 90vw; flex-grow: 1"
+    >
+      <template v-slot:[`item.status`]="{ item }">
+        <v-icon
+          :color="item.status ? 'success' : 'error'"
+          :icon="item.status ? 'mdi-check-circle-outline' : 'mdi-close-circle-outline'"
+        ></v-icon>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-btn
+          variant="text"
+          class="mr-4"
+          icon="mdi-pencil"
+          v-tooltip="'Edit Project'"
+          @click="onOpenProjectFormDialog(item)"
+        >
+        </v-btn>
+      </template>
+      <!-- table footer -->
+      <template v-slot:bottom>
+        <v-divider></v-divider>
+        <div class="d-flex ml-auto mr-10 mt-6">
+          <div class="mt-2 mr-2">Items per page:</div>
+          <v-select
+            variant="outlined"
+            item-title="label"
+            item-value="value"
+            density="compact"
+            :value="filters.itemsPerPage"
+            @update:model-value="
+              (itemsPerPage) => {
+                filters = {
+                  ...filters,
+                  itemsPerPage,
+                  page: 1,
+                }
+              }
+            "
+            :items="itemsPerPageDropdown"
+            width="90px"
+            :disabled="!data?.pagination.totalItems"
+          >
+          </v-select>
+          <div v-if="data?.pagination.totalItems" class="mt-2 ml-2">
+            {{ (filters.page - 1) * filters.itemsPerPage + 1 }} of
+            {{ data?.pagination.totalItems || '...' }}
+          </div>
+          <div v-else class="mt-2 ml-2">-</div>
+        </div>
+      </template>
+
+      <template v-slot:loading>
+        <table-loader :headers="headers"></table-loader>
+      </template>
+    </v-data-table-server>
+
+    <v-pagination
+      v-model="filters.page"
+      :length="data?.pagination?.totalPages"
+      :total-visible="7"
+      class="mt-10"
+    ></v-pagination>
+
+    <v-dialog v-model="projectFormDialog.open" max-width="700px" max-height="500px" persistent>
+      <div class="dialog-wrapper scrollable-dialog">
+        <project-form
+          :project="projectFormDialog.project"
+          @reset="onFiltersReset('project')"
+          @close="onCloseProjectFormDialog"
+        ></project-form>
+      </div>
+    </v-dialog>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+import { useQuery, useQueryClient } from '@tanstack/vue-query'
+import { useBaseStore } from '@/stores/base'
+import { debounce } from 'lodash'
+
+const baseStore = useBaseStore()
+const { itemsPerPageDropdown } = baseStore
+const projectFormDialog = ref({
+  open: false,
+  project: null,
+})
+
+const filters = ref({
+  page: 1,
+  itemsPerPage: 10,
+  title: null,
+})
+
+const headers = [
+  {
+    title: 'Name',
+    key: 'name',
+    sortable: false,
+  },
+
+  {
+    title: 'Description',
+    key: 'description',
+    sortable: false,
+  },
+
+  {
+    title: 'Status',
+    key: 'status',
+    sortable: false,
+  },
+  {
+    title: 'Actions',
+    key: 'actions',
+    align: 'start',
+    sortable: false,
+  },
+]
+
+const fetchProjects = async () => {
+  const res = await axios.get('/projects', {
+    params: {
+      limit: filters.value.itemsPerPage,
+      page: filters.value.page,
+      title: filters.value.title,
+    },
+  })
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 500)
+  })
+  return res.data
+}
+const queryClient = useQueryClient()
+
+const { isLoading, data } = useQuery({
+  queryKey: ['project', filters],
+  queryFn: fetchProjects,
+  retry: 0,
+})
+
+const onFiltersReset = async () => {
+  projectFormDialog.value = false
+
+  await queryClient.resetQueries({ queryKey: ['project'] })
+}
+
+const updateFilters = debounce((value) => {
+  filters.value = {
+    ...filters.value,
+    title: value,
+    page: 1,
+  }
+}, 300)
+
+async function onCloseProjectFormDialog() {
+  projectFormDialog.value = {
+    open: false,
+    project: null,
+  }
+}
+
+async function onOpenProjectFormDialog(project) {
+  projectFormDialog.value = {
+    open: true,
+    project: project || null,
+  }
+}
+</script>
 
 <style lang="scss" scoped></style>
